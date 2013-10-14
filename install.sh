500d1af52a0ebc3ecac0a8ccedd1cf7902ef9b75
Illia Polosukhin <ilblackdragon@gmail.com>
2013-10-14 12:22:15 -0700

    Added basic installation script for packages that are required. Note, that it supports only Debian systems and package list isn't compleate

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/env
+echo "This installer works only for Debian based systems"
+echo "Installing required packages"
+
+sudo apt-get install libatlas-dev libpng12-dev libfreetype6 libfreetype6-dev g++ libzmq-dev liblapack-dev gfortran python python-dev build-essential python-qt4
